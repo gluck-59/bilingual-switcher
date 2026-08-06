c1bac9e49670313723558995ca4482ab87b83a18
gluck <gluck59@gmail.com>
2026-08-06 09:45:46 +0500

    Repoint Sparkle update feed to gluck-59/bilingual-switcher

--- Casks/bilingual-switcher.rb
+++ Casks/bilingual-switcher.rb
@@ -2,10 +2,10 @@ cask "bilingual-switcher" do
   version "1.2.0"
   sha256 "f3d0469a6c277386f9f23b6e8335642c701a94368a8259131c327d1f2d9b597f"
 
-  url "https://github.com/komandakycto/bilingual-switcher/releases/download/v1.2.0/BilingualSwitcher.zip"
+  url "https://github.com/gluck-59/bilingual-switcher/releases/download/v1.2.0/BilingualSwitcher.zip"
   name "Bilingual Switcher"
   desc "Convert selected text between keyboard layouts with a hotkey"
-  homepage "https://github.com/komandakycto/bilingual-switcher"
+  homepage "https://github.com/gluck-59/bilingual-switcher"
 
   app "BilingualSwitcher.app"
 
