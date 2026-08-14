d0b49337a7bc1b99270e1966ca55e6d8040545b8
Ruben Yakunchev <86118746+gluck-59@users.noreply.github.com>
2026-08-14 13:25:28 +0000

    Update appcast and cask for 1.4 [skip ci]

--- Casks/bilingual-switcher.rb
+++ Casks/bilingual-switcher.rb
@@ -1,8 +1,8 @@
 cask "bilingual-switcher" do
-  version "1.3"
-  sha256 "e8a291546001e0fc8ef016521c5017a2a51708a39dc513a4e933451dfe74a5ff"
+  version "1.4"
+  sha256 "3a8471609755b093eaf0e4ec951886a262d7dd1bd4b418726a11de16baaed00b"
 
-  url "https://github.com/gluck-59/bilingual-switcher/releases/download/v1.3/BilingualSwitcher.zip"
+  url "https://github.com/gluck-59/bilingual-switcher/releases/download/v1.4/BilingualSwitcher.zip"
   name "Bilingual Switcher"
   desc "Convert selected text between keyboard layouts with a hotkey"
   homepage "https://github.com/gluck-59/bilingual-switcher"
